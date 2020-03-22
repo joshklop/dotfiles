6649f4aa1f84820fb27522cf1f4e25cecff2607f
Josh Klopfenstein <joshklop10@gmail.com>
2020-03-21 19:05:42 -0500

    added warning and netctl restart

--- switch_to_wifi.sh
+++ switch_to_wifi.sh
@@ -4,4 +4,6 @@ sudo systemctl stop dhcpcd.service
 sudo systemctl disable dhcpcd.service
 sudo rm -f /var/lib/dhcpcd/*.lease
 sudo rm -f /etc/netctl/wlo1*
-echo "REBOOT THE SYSTEM TO APPLY CHANGES"
+sudo systemctl restart netctl
+sudo systemctl enable netctl
+echo "\nYou may need to reboot to apply changes!\n"
