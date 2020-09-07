2c52f371ea570c56f7396d6039417076a76d36fc
Josh Klopfenstein <joshklop10@gmail.com>
2020-09-07 15:33:30 -0500

    Add useful options to trizen command
    `--needed` tells trizen to only upgrade out-of-date packages.
    `-c` tells trizen to clean pacman and trizen cache directories.

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -2,7 +2,7 @@
 
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
 sudo pacman -Syu
-trizen -Syu --aur --noconfirm
+trizen -Syu --aur --noconfirm --needed -c
 sudo pacman -Qqen > $HOME/.pkglist.txt
 sudo pacman -Qqem > $HOME/.foreignpkglist.txt
 pip list > $HOME/.python_pkglist.txt
