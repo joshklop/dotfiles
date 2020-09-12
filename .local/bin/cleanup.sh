01bf0951009e19c286ed749f590a5899e4fd3007
Josh Klopfenstein <joshklop10@gmail.com>
2020-09-12 08:57:36 -0500

    pacman: only upgrade out-of-date packages

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
-sudo pacman -Syu
 trizen -Syu --aur --noconfirm --needed -c
+sudo pacman -Syu --needed
 sudo pacman -Qqen > $HOME/.pkglist.txt
 sudo pacman -Qqem > $HOME/.foreignpkglist.txt
 pip list > $HOME/.python_pkglist.txt
