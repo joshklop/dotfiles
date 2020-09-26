e4e466e5cfc0aeb7fc23273409f5f314c381dfc1
Josh Klopfenstein <joshklop10@gmail.com>
2020-09-25 22:33:26 -0500

    Remove dangerous --noconfirm option

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -2,7 +2,7 @@
 
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
 sudo pacman -Syu --needed
-yay -Syu --aur --noconfirm --needed -c
+yay -Syu --aur --needed -c
 sudo pacman -Qqen > $HOME/.pkglist.txt
 sudo pacman -Qqem > $HOME/.foreignpkglist.txt
 pip list > $HOME/.python_pkglist.txt
