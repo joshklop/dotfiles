a068c0d437691ca1eb8a6179c7cffee160c1130b
Josh Klopfenstein <joshklop10@gmail.com>
2020-09-28 09:24:18 -0500

    Remove orphaned packages in cleanup script

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
+pacman -Rns $(pacman -Qtdq)  # Remove orphaned packages
 sudo pacman -Syu --needed
 yay -Syu --aur --needed -c
 sudo pacman -Qqen > $HOME/.pkglist.txt
