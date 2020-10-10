0509e9ab539301260694dc1c949ea2f6bc330d6a
Josh Klopfenstein <joshklop10@gmail.com>
2020-10-09 22:20:27 -0500

    Make variable names clearer

--- .local/bin/git_unsaved_changes.sh
+++ .local/bin/git_unsaved_changes.sh
@@ -4,17 +4,18 @@ repos=($HOME/code/* $HOME/cyberpatriot)
 
 # This StackOverflow answer helped with the colors
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-MAIN='\033[1;34m'
-RED='\033[0;31m'
-NC='\033[0m'  # No color
+PREFIX='\033['
+BLUE="${PREFIX}1;34m"
+RED="${PREFIX}0;31m"
+RESET="${PREFIX}0m"  # No color
 
 echo
 for i in "${repos[@]}"; do
   if [[ -d $i/.git ]]; then
-    echo -e "${MAIN}$i${NC}"
+    echo -e "${BLUE}$i${RESET}"
     behind=$(git -C $i rev-list --left-right --count origin/master...master | cut -f 1)
     ahead=$(git -C $i rev-list --left-right --count origin/master...master | cut -f 2)
-    echo -e "master is ${RED}$ahead${NC} commits ahead and ${RED}$behind${NC} commits behind origin/master."
+    echo -e "master is ${RED}$ahead${RESET} commits ahead and ${RED}$behind${RESET} commits behind origin/master."
     git -C $i st -s
     echo
   fi
