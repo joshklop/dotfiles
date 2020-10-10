935ef14e2f470211fcf29d15e1c1e2077b1f4bbe
Josh Klopfenstein <joshklop10@gmail.com>
2020-10-10 10:51:09 -0500

    Automatically push changes in /etc to backup repo

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -5,7 +5,7 @@ WHITE="${PREFIX}1;37m"
 GREEN="${PREFIX}1;32m"
 RESET="${PREFIX}0m"
 
-bwbackup.sh && borgbackup.sh
+bwbackup.sh && borgbackup.sh && cd /etc && sudo git push && cd -
 
 echo
 echo -e "${GREEN}* ${WHITE}Make sure you umount!${RESET}"
