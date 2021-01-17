0b62f944f8933f796fa3df1c05ba92c020518253
Josh Klopfenstein <joshklop10@gmail.com>
2021-01-17 14:19:19 -0600

    Add batchecker.sh

---
+++ .local/bin/batchecker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+  bat=$(upower -i $(upower -e | grep -i bat) | grep percentage | cut -d ':' -f 2 | xargs)
+  bat=${bat%\%}
+  if [[ $bat -le 10 ]]; then
+    notify-send --urgency="critical" "WARNING: battery charge is <= 10%"
+  elif [[ $bat -ge 90 ]]; then
+    notify-send --urgency="critical" "WARNING: battery charge is >= 90%"
+  fi
+  sleep 300;
+done
