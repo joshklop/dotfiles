b5e61d3fc8af03bf194606a3021a0372c7e309a9
Josh Klopfenstein <joshklop10@gmail.com>
2021-01-17 14:20:05 -0600

    Use paru instead of yay for AUR helper

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -2,7 +2,7 @@
 
 pacman -Rns $(pacman -Qtdq)  # Remove orphaned packages
 sudo pacman -Syu --needed
-yay -Syu --aur --needed -c
+paru -Syu --aur --needed -c
 sudo pacman -Qqen > $HOME/.pkglist.txt
 sudo pacman -Qqem > $HOME/.foreignpkglist.txt
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
