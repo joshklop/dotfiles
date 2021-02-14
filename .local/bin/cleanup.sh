4e6f8b786d42c2e78a0ec6dbd389d9304776ac56
Josh Klopfenstein <joshklop10@gmail.com>
2021-02-14 14:16:24 -0600

    Fix bug in cleanup.sh

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-sudo pacman -Rns $(pacman -Qtdq)  # Remove orphaned packages
-sudo pacman -Syu --needed
-paru -Syu --aur --needed -c
+sudo pacman -Syu --clean
+paru -Syu --aur --clean
 pacman -Qqen > $HOME/.pkglist.txt
 pacman -Qqem > $HOME/.foreignpkglist.txt
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
