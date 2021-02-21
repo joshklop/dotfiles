f993030a2e33fd140beda98dbe25f8dd18a2aae7
Josh Klopfenstein <joshklop10@gmail.com>
2021-02-21 15:28:09 -0600

    cleanup.sh: bug fix

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo pacman -Syu --clean
-paru -Syu --aur --clean
+sudo pacman -Syu
+paru -Syu --aur
 pacman -Qqen > $HOME/.pkglist.txt
 pacman -Qqem > $HOME/.foreignpkglist.txt
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
