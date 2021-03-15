52690b264b2bde75417b007bac6110f9952c87b9
Josh Klopfenstein <joshklop10@gmail.com>
2021-03-14 22:24:34 -0500

    Use slock instead of i3-lock

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -8,16 +8,7 @@ amixer set Master mute
 echo "Pausing music"
 playerctl pause
 
-echo "locking screen" \
-        &&  i3lock \
-                --ignore-empty-password \
-                --nofork \
-                --show-failed-attempts \
-                --indicator \
-                --clock \
-                --color=313030ff \
-                --insidecolor=807369ff \
-        && echo "unlocking"
+slock
 
 echo "Unmuting audio"
 amixer set Master unmute
