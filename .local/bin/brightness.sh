84a495e9d77ed7059186f5844d94e66f10868f60
Josh Klopfenstein <joshklop10@gmail.com>
2021-05-09 16:08:51 -0500

    Change brightness less

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -3,9 +3,9 @@
 brightness="/sys/class/backlight/intel_backlight/brightness"
 current=$(cat $brightness)
 if [[ $1 = "+" ]]; then
-  let "new = $current + 500"
+  let "new = $current + 300"
   echo $new > $brightness
 elif [[ $1 = "-" ]]; then
-  let "new = $current - 500"
+  let "new = $current - 300"
   echo $new > $brightness
 fi
