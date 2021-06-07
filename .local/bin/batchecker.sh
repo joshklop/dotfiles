97f906fd5343c1646087f5cd20597a0db1b4dbb8
Josh Klopfenstein <joshklop10@gmail.com>
2021-06-07 16:09:51 -0500

    Check if battery is being charged

--- .local/bin/batchecker.sh
+++ .local/bin/batchecker.sh
@@ -2,10 +2,11 @@
 
 while true; do
   bat=$(upower -i $(upower -e | grep -i bat) | grep percentage | cut -d ':' -f 2 | xargs)
+  state=$(upower -i $(upower -e | grep -i bat) | grep state | cut -d ':' -f 2 | xargs)
   bat=${bat%\%}
-  if [[ $bat -le 10 ]]; then
+  if [[ $bat -le 10 && $state != "charging" ]]; then
     notify-send --urgency="critical" "WARNING: battery charge is <= 10%"
-  elif [[ $bat -ge 90 ]]; then
+  elif [[ $bat -ge 90 && $state == "charging" ]]; then
     notify-send --urgency="critical" "WARNING: battery charge is >= 90%"
   fi
   sleep 300;
