1cacabaa36a062c2069c720e9bd8167f4a779943
Josh Klopfenstein <joshklop10@gmail.com>
2021-06-13 18:31:06 -0500

    Upgrade pip

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -4,4 +4,5 @@ sudo pacman -Syu
 paru -Syu --aur
 pacman -Qqen > $HOME/.pkglist.txt
 pacman -Qqem > $HOME/.foreignpkglist.txt
+python3 -m pip install --upgrade pip
 pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
