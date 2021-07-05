3b30e2095e11549b0ab5011efe092dfbe8c08c30
Josh Klopfenstein <joshklop10@gmail.com>
2021-07-05 06:59:33 -0500

    Remove infinite loop when already logged in

--- .local/bin/bwbackup.sh
+++ .local/bin/bwbackup.sh
@@ -2,16 +2,8 @@
 
 BACKUP="$HOME/documents/bw.json"
 
-function force(){
-  while true; do
-    if $1; then
-      break
-    fi
-  done
-}
-
-force "bw login" \
-  && force "bw export --format json --output $BACKUP" \
+bw login \
+  && bw export --format json --output $BACKUP \
   && bw logout \
   && gpg --encrypt --sign --recipient joshklop10@gmail.com $BACKUP \
   && rm $BACKUP
