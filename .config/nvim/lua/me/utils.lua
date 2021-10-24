b85944aeccf656eaea3c14a0c8c02f01fa2a7d86
Josh Klopfenstein <joshklop10@gmail.com>
2021-10-23 20:13:07 -0500

    nvim: improve dotfiles telescope picker

--- .config/nvim/lua/me/utils.lua
+++ .config/nvim/lua/me/utils.lua
@@ -2,29 +2,26 @@ local M = {}
 
 function M.find_dotfiles(opts)
     opts = opts or {}
+    opts.cwd = opts.cwd or vim.loop.cwd()
     local home = os.getenv('HOME')
+    local Path = require('plenary.path')
     opts.entry_maker = opts.entry_maker or function(entry)
-       -- TODO clean up so paths are relative
-       local path = home .. '/' .. entry
-       local display = '~/' .. entry
+       local path = Path:new(Path:new(home .. '/' .. entry):make_relative(opts.cwd)):normalize(opts.cwd)
        return {
            path = path,
-           value = display,
-           display = display,
-           ordinal = display
+           value = path,
+           display = path,
+           ordinal = path
        }
     end
-    local pickers = require('telescope.pickers')
-    local sorters = require('telescope.sorters')
-    local finders = require('telescope.finders')
     local custom_cmd = {
         'git', '--git-dir=' .. home .. '/.dotfiles', '--work-tree=' .. home,
         'ls-tree', '--full-tree', '-r', '--name-only', 'HEAD'
     }
-    pickers.new(opts, {
+    require('telescope.pickers').new(opts, {
         results_title = 'dotfiles',
-        finder = finders.new_oneshot_job(custom_cmd, opts),
-        sorter = sorters.get_fuzzy_file(),
+        finder = require('telescope.finders').new_oneshot_job(custom_cmd, opts),
+        sorter = require('telescope.sorters').get_fuzzy_file(),
         previewer = require('telescope.config').values.file_previewer(opts)
     }):find()
 end
