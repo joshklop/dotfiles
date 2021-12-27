ba56221e92fb77b7c4b958116902c6cd986d916d
Josh Klopfenstein <joshklop10@gmail.com>
2021-12-27 04:57:52 -0600

    pacmanfile: install

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -2,11 +2,7 @@
 
 sudo pacman -Syu
 paru -Syu --aur
-pacman -Qqen > $HOME/.pkglist.txt
-pacman -Qqem > $HOME/.foreignpkglist.txt
+pacmanfile sync
 python3 -m pip install --upgrade pip
-# Don't upgrade global python packages unless you have to
-# Prefer virtual environments. Plus, pip takes a while.
-# pip install --upgrade --user $(pip list --outdated --user --format=freeze | cut -f 1 -d =)
 nvim -c PackerUpdate
 sudo nvim -c PackerUpdate
