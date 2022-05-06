7cad6aa060aac08a91814056ef837481d0691c2c
Josh Klopfenstein <joshklop10@gmail.com>
2022-05-06 16:23:32 -0500

    info.sh: add battery charging state

--- .local/bin/info.sh
+++ .local/bin/info.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-cmd=$(upower -i $(upower -e | grep -i bat) | grep percentage | cut -d: -f2 | xargs)
+cmd=$(upower -i $(upower -e | grep -i bat) | awk '$1 ~ /percentage/ { print $2 }')
+state=$(upower -i $(upower -e | grep -i bat) | awk '$1 ~ /state/ { print $2 }')
+
+if [[ "$state" == 'discharging' ]]; then
+    state='unplugged'
+else
+    state='charging'
+fi
 
 time=$(date +%F\ %l:%M\ %p)
 
-notify-send "Battery: ${cmd}
+notify-send "Battery: ${cmd} ${state}
 Time: ${time}"
