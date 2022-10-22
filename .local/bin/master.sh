b41717c0a7353be62ba8305f4079a8ac4fbbfe46
Josh Klopfenstein <joshklop10@gmail.com>
2022-10-22 17:44:36 -0500

    Cleanup scripts

--- .local/bin/master.sh
+++ .local/bin/master.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 PREFIX='\033['
 GREEN="${PREFIX}1;32m"
@@ -10,15 +10,15 @@ function ready(){
   prompt=$1
   cmd=$2
   while true; do
-    if [[ $cmd == "" ]]; then
+    if [[ "$cmd" == "" ]]; then
       echo -e -n "$prompt [y/n] $RESET"; read resp
       [[ $resp =~ [yY] ]] && break
     else
       echo -e -n "$prompt [y/n] $RESET"; read resp
-      if [[ $resp =~ y ]]; then
-        $cmd
+      if [[ "$resp" =~ y ]]; then
+        zsh -c "$cmd"
         break
-      elif [[ $resp =~ n ]]; then
+      elif [[ "$resp" =~ n ]]; then
         echo -e "${RED}Skipping...${RESET}"
         break
       fi
@@ -27,13 +27,24 @@ function ready(){
 }
 
 ready "${GREEN}==>${RESET} ${BOLD}Have you deleted unnecessary files?${RESET}" 
-ready "${GREEN}==>${RESET} ${BOLD}Rank mirrors?${RESET}" "sudo reflector --country us --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
-ready "${GREEN}==>${RESET} ${BOLD}Update packages and neovim plugins?${RESET}" cleanup.sh
-ready "${GREEN}==>${RESET} ${BOLD}Run 'zinit self-update' and 'zinit update --parallel'.${RESET}"
+
+# TODO nohup + & may be useful for blindly running this in the background to speed things up
+ready "${GREEN}==>${RESET} ${BOLD}Rank mirrors?${RESET}" 'sudo reflector --country us --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist'
+
+ready "${GREEN}==>${RESET} ${BOLD}Update packages?${RESET}" 'sudo pacman -Syu; paru -Syu --aur; pacmanfile dump; python3 -m pip install --upgrade pip'
+
+ready "${GREEN}==>${RESET} ${BOLD}Update neovim plugins and treesitter parsers?${RESET}" 'nvim -c PackerUpdate; sudo nvim -c PackerUpdate'
+
+ready "${GREEN}==>${RESET} ${BOLD}Update zinit and plugins?${RESET}" 'source "$HOME/.local/share/zinit/zinit.git/zinit.zsh" && zinit self-update && zinit update --parallel'
+
 ready "${GREEN}==>${RESET} ${BOLD}Backup bitwarden?${RESET}" bwbackup.sh
+
 ready "${GREEN}==>${RESET} ${BOLD}Backup ${HOME}?${RESET}" borgbackup.sh
-ready "${GREEN}==>${RESET} ${BOLD}Backup /etc?${RESET}" "sudo etcbackup.sh"
+
+ready "${GREEN}==>${RESET} ${BOLD}Backup /etc?${RESET}" 'cd /etc && sudo git push && cd -'
+
 echo
 echo -e "${GREEN}* ${BOLD}Make sure you umount!${RESET}"
 echo
+
 ready "${GREEN}==>${RESET} ${BOLD}Commit and push your dotfiles.${RESET}"
